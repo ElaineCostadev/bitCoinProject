3fe039871dd3b5ba898f46654b30de5c63122685
elainecostadev <elainerscosta@hotmail.com>
2023-04-04 18:56:55 -0300

    :feat: configuracao inicial do APP para mostrar cada componente na tela principal

---
+++ App.js
@@ -0,0 +1,29 @@
+import { StyleSheet, Text, View, SafeAreaView, Platform, StatusBar } from 'react-native';
+import CurrentPrice from './src/components/CurrentPrice';
+import HistoryGraphic from './src/components/HistoryGraphic';
+import QuotationList from './src/components/QuotationList';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar
+        backgroundColor='#f50d41'
+        barStyle={'dark-content'}
+      
+      />
+      <CurrentPrice />
+      <HistoryGraphic />
+      <QuotationList />
+
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000000',
+    alignItems: 'center',
+    paddingTop: Platform.OS === 'android' ? 40 : 0
+  },
+});
