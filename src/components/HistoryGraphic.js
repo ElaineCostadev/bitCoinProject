75461b4479a2eb6a01ca175bded618e4a1fdd282
elainecostadev <elainerscosta@hotmail.com>
2023-04-04 18:55:21 -0300

    :feat: funcao criada para mostrar o historico das cotacoes

---
+++ src/components/HistoryGraphic.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import styles from '../styles/HistoryGraphic.style'
+
+const HistoryGraphic = () => {
+  return (
+    <View style={styles.contentGraphic}>
+      <Text>HistoryGraphic</Text>
+    </View>
+  )
+}
+
+export default HistoryGraphic
