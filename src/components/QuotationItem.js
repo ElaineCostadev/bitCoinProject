aec11a1e8960f39b8955a0f7b1e126d6bcb01373
elainecostadev <elainerscosta@hotmail.com>
2023-04-04 18:56:14 -0300

    :feat: funcao criada para mostrar cada item da cotacao

---
+++ src/components/QuotationItem.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+// import styles from '../styles/QuotationItem.style'
+
+const QuotationItem = () => {
+  return (
+    <View>
+      <Text>QuotationItem</Text>
+    </View>
+  )
+}
+
+export default QuotationItem
