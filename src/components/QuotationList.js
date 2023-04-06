eebe7f02c90e26acbfd3b012c4a3c76cc38133ec
elainecostadev <elainerscosta@hotmail.com>
2023-04-06 19:57:17 -0300

    :feat: incluindo props para enviar os dados da funcao filterDay

--- src/components/QuotationList.js
+++ src/components/QuotationList.js
@@ -1,47 +1,61 @@
-import { View, Text, ScrollView, TouchableOpacity } from 'react-native'
+import { View, Text, ScrollView, TouchableOpacity, FlatList } from 'react-native'
 import React, { Fragment } from 'react'
 import styles from '../styles/QuotationList.style'
+import QuotationItem from './QuotationItem'
 
-const QuotationList = () => {
+const QuotationList = ({filterDay, listTransactions}) => {
+  // const daysQuery = props.filterDay
   return (
     <Fragment>
       <View style={styles.filters}>
         <TouchableOpacity
           style={styles.buttonQuery}
-          onPress={() => {}}
+          onPress={() => { filterDay(7) }}
         >
-          <Text style={styles.textButtonQuery}>7 D</Text>
+          <Text style={styles.textButtonQuery}>7D</Text>
         </TouchableOpacity>
 
         <TouchableOpacity
           style={styles.buttonQuery}
-          onPress={() => {}}
+          onPress={() => { filterDay(15) }}
         >
-          <Text style={styles.textButtonQuery}>15 D</Text>
+          <Text style={styles.textButtonQuery}>15D</Text>
         </TouchableOpacity>
 
         <TouchableOpacity
           style={styles.buttonQuery}
-          onPress={() => {}}
+          onPress={() => { filterDay(30) }}
         >
-          <Text style={styles.textButtonQuery}>1 M</Text>
+          <Text style={styles.textButtonQuery}>1M</Text>
         </TouchableOpacity>
 
         <TouchableOpacity
           style={styles.buttonQuery}
-          onPress={() => {}}
+          onPress={() => { filterDay(90) }}
         >
-          <Text style={styles.textButtonQuery}>3 M</Text>
+          <Text style={styles.textButtonQuery}>3M</Text>
         </TouchableOpacity>
 
         <TouchableOpacity
           style={styles.buttonQuery}
-          onPress={() => {}}
+          onPress={() => { filterDay(180) }}
         >
-          <Text style={styles.textButtonQuery}>6 M</Text>
+          <Text style={styles.textButtonQuery}>6M</Text>
         </TouchableOpacity>
       </View>
-      {/* <ScrollView></ScrollView> */}
+      <ScrollView>
+        <FlatList 
+          data={listTransactions}
+          renderItem={({ item }) => {
+            return (
+              <QuotationItem valor={item.valor} data={item.data} />
+            )
+
+          }}
+        
+        />
+
+      </ScrollView>
     </Fragment>
   )
 }
